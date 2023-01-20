9ad8f82c1f4e58e987ca5356d47f4a1bc1053236
Divya-Konala <87460968+Divya-Konala@users.noreply.github.com>
2023-01-20 21:36:59 +0530

    commit

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 //your code here
+let inputBox= document.createElement("input");
+inputBox.setAttribute("type","text");
+inputBox.setAttribute("id","evaluatedText");
+document.body.append(inputBox);
+let h3Ele=document.createElement("h3");
+h3Ele.setAttribute("id","wordCount");
+document.body.append(h3Ele);
+
+document.getElementById("evaluatedText").addEventListener("keyup",()=>{
+        let word_count=inputBox.value.length;
+        document.getElementById("wordCount").innerText=word_count;
+    })
