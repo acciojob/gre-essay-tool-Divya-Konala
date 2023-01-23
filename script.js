6f98dc8301f231fa722c088ea672257b2af0654a
Divya-Konala <87460968+Divya-Konala@users.noreply.github.com>
2023-01-23 17:15:38 +0530

    word count

--- script.js
+++ script.js
@@ -5,9 +5,14 @@ inputBox.setAttribute("id","evaluatedText");
 document.body.append(inputBox);
 let h3Ele=document.createElement("h3");
 h3Ele.setAttribute("id","wordCount");
+h3Ele.innerText='0';
 document.body.append(h3Ele);
+let arrOfWords;
 
 document.getElementById("evaluatedText").addEventListener("keyup",()=>{
-        let word_count=inputBox.value.length;
+     let wordArr=inputBox.value.trim().split(/\s+/);
+     console.log(wordArr);
+     let word_count=wordArr.length;
         document.getElementById("wordCount").innerText=word_count;
     })
+
